af5a5c85bc4216f5498a93df03549473c9c7ff83
Fent Jani <fent.jani@gmail.com>
2023-05-12 21:36:23 +0200

    Deploy to gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,8 +11,8 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
-  base: config.site.base_path ? config.site.base_path : "/",
+  site: config.site.base_url ? config.site.base_url : "https://antlify.github.io",
+  base: config.site.base_path ? config.site.base_path : "/astro",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   integrations: [
     react(),
